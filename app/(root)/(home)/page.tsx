b9fba11eded3babe73fe3d7ccd7c2863de85e13d
“Daffa <mdaffakha@gmail.com>
2025-05-05 22:14:31 +0700

    feat(MEET-3): implement top navbar with user avatar and logo

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <h1 className="text-3xl font-bold">
-        Home
+        Home Page
       </h1>
     </section>
   )
