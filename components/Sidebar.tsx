f3ba124682f2314efbf6bc18bd181ecc2ac99080
Muhamad Daffa <131875089+dapaii@users.noreply.github.com>
2025-05-05 22:22:13 +0700

    Feat/navbar component (#3)
    * chore(MEET-1): setup initial folder and file structure for Next.js app
    
    * feat(MEET-2): add responsive sidebar layout with navigation links
    
    * feat(MEET-3): implement top navbar with user avatar and logo
    
    ---------
    
    Co-authored-by: “Daffa <“mdaffakha@gmail.com”>

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,11 +12,11 @@ const Sidebar = () => {
   const pathname = usePathname()
   return (
     
-    <section className='sticky left-0 top-0 h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
-        <div className="flex flex-col gap-6">
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+        <div className="flex flex-1 flex-col gap-6">
           {/* sidbarLinks => constants */}
           {sidebarLinks.map((link) =>{
-            const isActive = pathname === link.route || pathname.startsWith(link.route);
+            const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
           
           return(
             <Link
@@ -38,5 +38,7 @@ const Sidebar = () => {
           })}
         </div>
     </section>
+  )
+}
 
 export default Sidebar
